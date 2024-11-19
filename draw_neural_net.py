84607626fe36023599cd1aba7065ecfccaa442b0
xsheu <49124086+xsheu@users.noreply.github.com>
2024-11-19 21:54:58 +0800

    Add files via upload
    correct version

--- draw_neural_net.py
+++ draw_neural_net.py
@@ -1,3 +1,9 @@
+# 請不要動
+import subprocess
+import sys
+subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib"])
+# 請不要動
+
 import matplotlib.pyplot as plt
 
 def draw_neural_net(ax, left, right, bottom, top, layer_sizes):
@@ -28,7 +34,7 @@ def draw_neural_net(ax, left, right, bottom, top, layer_sizes):
     # Nodes
     for n, layer_size in enumerate(layer_sizes):
         layer_top = v_spacing*(layer_size - 1)/2. + (top + bottom)/2.
-        for m in xrange(layer_size):
+        for m in range(layer_size):
             circle = plt.Circle((n*h_spacing + left, layer_top - m*v_spacing), v_spacing/4.,
                                 color='w', ec='k', zorder=4)
             ax.add_artist(circle)
@@ -36,8 +42,14 @@ def draw_neural_net(ax, left, right, bottom, top, layer_sizes):
     for n, (layer_size_a, layer_size_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
         layer_top_a = v_spacing*(layer_size_a - 1)/2. + (top + bottom)/2.
         layer_top_b = v_spacing*(layer_size_b - 1)/2. + (top + bottom)/2.
-        for m in xrange(layer_size_a):
-            for o in xrange(layer_size_b):
+        for m in range(layer_size_a):
+            for o in range(layer_size_b):
                 line = plt.Line2D([n*h_spacing + left, (n + 1)*h_spacing + left],
                                   [layer_top_a - m*v_spacing, layer_top_b - o*v_spacing], c='k')
                 ax.add_artist(line)
+
+fig = plt.figure(figsize=(7.5, 7.5))
+ax = fig.gca()
+ax.axis('off')
+draw_neural_net(ax, .1, .9, .1, .9, [4, 5, 2])
+plt.show()
